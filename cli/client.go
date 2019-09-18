3f05d12eda43fdce45b395a822d2da1ed0d39598
Shitaibin <hz_stb@163.com>
2019-09-18 15:41:14 +0800

    refactor(cli): split cli to struct defination and chaincode operation

---
+++ cli/client.go
@@ -0,0 +1,78 @@
+package cli
+
+import (
+	"log"
+	"os"
+
+	"github.com/hyperledger/fabric-sdk-go/pkg/client/channel"
+	"github.com/hyperledger/fabric-sdk-go/pkg/client/resmgmt"
+	"github.com/hyperledger/fabric-sdk-go/pkg/core/config"
+	"github.com/hyperledger/fabric-sdk-go/pkg/fabsdk"
+)
+
+type Cli struct {
+	// Fabric network information
+	ConfigPath string
+	OrgName    string
+	OrgAdmin   string
+	OrgUser    string
+
+	// sdk clients
+	sdk *fabsdk.FabricSDK
+	rc  *resmgmt.Client
+	cc  *channel.Client
+
+	// Same for each peer
+	ChannelID string
+	CCID      string // chaincode ID, eq name
+	CCPath    string // chaincode source path, 是GOPATH下的某个目录
+	CCGoPath  string // GOPATH used for chaincode
+}
+
+func New(cfg, org, admin, user string) *Cli {
+	c := &Cli{
+		ConfigPath: cfg,
+		OrgName:    org,
+		OrgAdmin:   admin,
+		OrgUser:    user,
+
+		CCID:      "example2",
+		CCPath:    "github.com/hyperledger/fabric-samples/chaincode/chaincode_example02/go/", // 相对路径是从GOPAHT/src开始的
+		CCGoPath:  os.Getenv("GOPATH"),
+		ChannelID: "mychannel",
+	}
+
+	// create sdk
+	sdk, err := fabsdk.New(config.FromFile(c.ConfigPath))
+	if err != nil {
+		log.Panicf("failed to create fabric sdk: %s", err)
+	}
+	c.sdk = sdk
+	log.Println("Initialized fabric sdk")
+
+	c.rc, c.cc = NewSdkClient(sdk, c.ChannelID, c.OrgName, c.OrgAdmin, c.OrgUser)
+	return c
+}
+
+// NewSdkClient create resource client and channel client
+func NewSdkClient(sdk *fabsdk.FabricSDK, channelID, orgName, orgAdmin, OrgUser string) (rc *resmgmt.Client, cc *channel.Client) {
+	var err error
+
+	// create rc
+	rcp := sdk.Context(fabsdk.WithUser(orgAdmin), fabsdk.WithOrg(orgName))
+	rc, err = resmgmt.New(rcp)
+	if err != nil {
+		log.Panicf("failed to create resource client: %s", err)
+	}
+	log.Println("Initialized resource client")
+
+	// create cc
+	ccp := sdk.ChannelContext(channelID, fabsdk.WithUser(OrgUser))
+	cc, err = channel.New(ccp)
+	if err != nil {
+		log.Panicf("failed to create channel client: %s", err)
+	}
+	log.Println("Initialized channel client")
+
+	return rc, cc
+}
